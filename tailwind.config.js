ce129ea1eb4c972e5f6abe5e314b6dfdd2b91873
ericramones1253@gmail.com <ericramones1253@gmail.com>
2025-02-01 23:36:42 +0800

    Updated portfolio files

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  purge: ["./index.html", "./src/**/*.js"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
